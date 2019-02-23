c2d4fca680cd85d56f48d5ef7fe03776a05fe237
Ricardo Casanova <ricardo@pijp.eu>
2019-02-23 22:49:42 +0100

    Add Date extension file

---
+++ iMusic/iMusic/Extensions/Date/Date.swift
@@ -0,0 +1,32 @@
+//
+//  Date.swift
+//  iMusic
+//
+//  Created by Ricardo Casanova on 23/02/2019.
+//  Copyright © 2019 Pijp. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    public static func getISODateWithString(_ stringDate: String) -> Date? {
+        let isoFormatter = DateFormatter()
+        isoFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        isoFormatter.locale = Locale(identifier: "en_US_POSIX")
+        return isoFormatter.date(from: stringDate)
+    }
+    
+    public func getStringyyyyFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        return dateFormatter.string(from: self)
+    }
+    
+    public func getStringMMMddyyyyFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd, yyyy"
+        return dateFormatter.string(from: self)
+    }
+    
+}
