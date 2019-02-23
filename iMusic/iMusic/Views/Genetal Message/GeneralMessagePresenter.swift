f8c5f1176908523a7004b67fbf7cd3bce6e5eb0c
Ricardo Casanova <ricardo@pijp.eu>
2019-02-23 22:42:12 +0100

    Add GeneralMessage file

---
+++ iMusic/iMusic/Views/Genetal Message/GeneralMessagePresenter.swift
@@ -0,0 +1,43 @@
+//
+//  GeneralMessagePresenter.swift
+//
+//  Created by Ricardo Casanova on 07/09/2018.
+//
+
+import Foundation
+
+/**
+ * Enum to manage all the possible messages types
+ * For now I'm using only NoInternetConnection type but it's possible to extend this in the future
+ */
+enum GeneralMessageType {
+    case NoInternetConnection
+}
+
+class GeneralMessagePresenter {
+    
+    private weak var view: GeneralMessageViewInjection?
+    private let type: GeneralMessageType
+    
+    // MARK - Lifecycle
+    init(view: GeneralMessageViewInjection, type: GeneralMessageType) {
+        self.view = view
+        self.type = type
+    }
+    
+}
+
+extension GeneralMessagePresenter: GeneralMessagePresenterDelegate {
+    
+    /**
+     * View did appear
+     */
+    func viewDidAppear() {
+        switch type {
+        case .NoInternetConnection:
+            view?.load(title: "Without connection to the network.", message: "ABA Music needs to connect to the internet.\nCheck the connections and try again.")
+            break
+        }
+    }
+
+}
