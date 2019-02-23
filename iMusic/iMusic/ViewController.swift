fafc78ed8191b598ec3df2dec8acb6ef7c22f7c5
Ricardo Casanova <ricardo@pijp.eu>
2019-02-23 22:02:14 +0100

    First commit

---
+++ iMusic/iMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Ricardo Casanova on 23/02/2019.
+//  Copyright © 2019 Pijp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
