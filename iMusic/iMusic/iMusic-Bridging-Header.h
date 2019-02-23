217c285ac85a71ce1f532776abfcb7b07d41ba6f
Ricardo Casanova <ricardo@pijp.eu>
2019-02-23 22:12:15 +0100

    Add iMusic-Bridging-Header header file

---
+++ iMusic/iMusic/iMusic-Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  iMusic-Bridging-Header.h
+//  iMusic
+//
+//  Created by Ricardo Casanova on 23/02/2019.
+//  Copyright © 2019 Pijp. All rights reserved.
+//
+
+#ifndef iMusic_Bridging_Header_h
+#define iMusic_Bridging_Header_h
+
+//__ Reachability
+#import "Reachability.h"
+
+//__ SVProgressHUD
+#import <SVProgressHUD/SVProgressHUD.h>
+
+//__ Haneke
+#import <Haneke/Haneke.h>
+
+#endif /* iMusic_Bridging_Header_h */
